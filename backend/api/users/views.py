cf514ab59448b5918f5d63d2191dfcf4b52200ac
bsammor <sammor.basel@gmail.com>
2020-10-02 01:21:47 +0200

    pushed latest vscode config

--- backend/api/users/views.py
+++ backend/api/users/views.py
@@ -8,13 +8,14 @@ from users.serializers import UserSerializer
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
 
+
 # Create your views here.
 @api_view(['POST'])
 def register(request):
     data = JSONParser().parse(request)
     serializer = UserSerializer(data=data)
-    
+
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data, status=201)
-    return Response({"Registration" : serializer.errors}, status=400)
+    return Response({"Registration": serializer.errors}, status=400)
