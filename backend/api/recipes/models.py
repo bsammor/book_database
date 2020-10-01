f9c3f4b80a08be856431833117d03706a0cfb84d
bsammor <sammor.basel@gmail.com>
2020-10-01 04:33:28 +0200

    started initial work on reactjs

--- backend/api/recipes/models.py
+++ backend/api/recipes/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.conf import settings
 from django.db.models import JSONField
 
 # Create your models here.
 class Recipe(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.CASCADE)
     title = models.CharField( max_length=50)
     recipe_image = models.ImageField(null=True, upload_to='recipes')
     description = models.CharField(max_length=250)
